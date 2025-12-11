f8be36552e54d3e58d41ab952318f7675755d2cd
RoastedChestnut <chestnut.roasted@outlook.com>
2025-12-11 10:52:15 +0800

    chore: bump version to 0.3.0

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Orchid.MixProject do
   def project do
     [
       app: :orchid,
-      version: "0.2.2",
+      version: "0.3.0",
       build_path: "_build",
       deps_path: "deps",
       lockfile: "mix.lock",
