e872fe75d4f8b16d1ea34ab6711c76eb99a67a45
RoastedChestnut <chestnut.roasted@outlook.com>
2026-01-12 10:23:46 +0800

    fix: make Orchid.Pipeline.run/2's return type fit declaration

--- lib/orchid/pipeline.ex
+++ lib/orchid/pipeline.ex
@@ -1,12 +1,13 @@
 defmodule Orchid.Pipeline do
-  @doc """
-  Run pipeline at request level(not step).
-  """
-  @spec run([module()], Orchid.Operon.Request.t()) ::
-          Orchid.Operon.Response.t() | {:error, term()}
+  @doc "Run pipeline at request level(not step)."
+  alias Orchid.Operon.{Request, Response}
+
+  @spec run([module()], Request.t()) :: Response.t()
   def run(stack, request), do: dispatch(stack, request)
 
-  defp dispatch([], _req), do: {:error, :no_sink_middleware}
-  defp dispatch([sink], req), do: sink.call(req, fn _ -> {:error, :reached_sink_end} end)
+  defp dispatch([], _req), do: build_err(:no_sink_middleware)
+  defp dispatch([sink], req), do: sink.call(req, fn _ -> build_err(:reached_sink_end) end)
   defp dispatch([operon | rest], req), do: operon.call(req, &dispatch(rest, &1))
+
+  defp build_err(reason), do: %Response{payload: {:error, %Orchid.Error{reason: reason}}}
 end
