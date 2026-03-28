32b8d589c778cbadc8529fa6728cbece3c774df4
RoastChestnut <30802664+GES233@users.noreply.github.com>
2026-03-28 21:47:12 +0800

    Refactor repo.ex to enhance storage callbacks and types
    Refactor callbacks to include store_ref and update types. Add new modules for Blob, GC, and Pickle with relevant callbacks.

--- lib/orchid/repo.ex
+++ lib/orchid/repo.ex
@@ -4,17 +4,43 @@ defmodule Orchid.Repo do
   Used to store large volumes of data unsuitable for direct transmission via
   Param (such as audio waveforms or model weights).
 
-  Implementation within Orchid is not currently envisaged, though other applications
-  may implement this protocol and invoke it via custom hooks.
+  Implementation within Orchid is not envisaged, though other applications
+  may implement this protocol and invoke it via custom hooks/operons/plugins.
   """
+  # Integrate OrchidStratum's design.
 
-  @type key :: term()
+  @type store_ref :: term()
+  @type key :: binary()
   @type value :: term()
-  @type opts :: keyword()
 
-  @callback put(value(), opts()) :: {:ok, key()} | {:error, term()}
+  @callback put(store_ref(), key(), value()) :: :ok
 
-  @callback get(key()) :: {:ok, value()} | {:error, term()}
+  @callback get(store_ref(), key()) :: {:ok, value()} | :miss
 
-  @callback delete(key()) :: :ok | {:error, term()}
+  @callback delete(store_ref(), key()) :: :ok
+
+  defmodule Blob do
+    # Used for content-addressed storage
+    @callback exists?(store_ref(), key()) :: boolean()
+  end
+
+  defmodule GC do
+    @callback garbage_collect(store_ref(), opts :: term()) :: :ok
+  end
+
+  defmodule Pickle do
+    # Yeah, Python's pickle
+    # How to ensure its safety?
+
+    # opts: delete content in storage when serialization done?
+    @callback serialize(store_ref(), condition :: :whole | {:partial, condition :: term()}, opts :: keyword())
+              :: {:ok, serialized :: term()} | {:error, reason :: term()}
+
+    @callback deserialize(store_ref(), serialized :: term()) :: :ok | {:error, reason :: term()}
+  end
+
+  # defmodule Native do
+  #   # Implement some zero-copy feature with NIF
+  #   # maybe...
+  # end
 end
