04c18fa4c5b291195e3b757dc0b18272c21acf63
RoastedChestnut <chestnut.roasted@outlook.com>
2026-03-28 15:33:49 +0800

    chore: Update version to 0.5.8

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Orchid.MixProject do
   def project do
     [
       app: :orchid,
-      version: "0.5.7-fix",
+      version: "0.5.8",
       build_path: "_build",
       deps_path: "deps",
       lockfile: "mix.lock",
